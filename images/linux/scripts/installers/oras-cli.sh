b245b7d94d7a9778dc8f95f5a2a7940231dd74a2
manojvazirani <43583701+manojvazirani@users.noreply.github.com>
2021-07-08 11:08:00 -0400

    Update oras-cli.sh

--- images/linux/scripts/installers/oras-cli.sh
+++ images/linux/scripts/installers/oras-cli.sh
@@ -7,8 +7,12 @@
 source $HELPER_SCRIPTS/install.sh
 
 # Determine latest ORAS CLI version
+curl -V
+jq --version
+
 ORAS_CLI_LATEST_VERSION_URL=https://api.github.com/repos/oras-project/oras/releases/latest
-ORAS_CLI_DOWNLOAD_URL=$(curl -sfL $ORAS_CLI_LATEST_VERSION_URL | jq -r '.assets[].browser_download_url | select(endswith("linux_amd64.tar.gz"))')
+#ORAS_CLI_DOWNLOAD_URL=$(curl -sfL $ORAS_CLI_LATEST_VERSION_URL | jq -r '.assets[].browser_download_url | select(endswith("linux_amd64.tar.gz"))')
+ORAS_CLI_DOWNLOAD_URL=https://github.com/oras-project/oras/releases/download/v0.12.0/oras_0.12.0_linux_amd64.tar.gz
 ORAS_CLI_ARCHIVE=$(basename $ORAS_CLI_DOWNLOAD_URL)
 
 # Install ORAS CLI
